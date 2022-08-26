1661d98f83daba26ded051861a14e31be04b696b
Michael Tan <michael@rych.io>
2022-08-26 14:34:19 +0800

    test ideal-image

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,6 +14,19 @@ module.exports = {
     organizationName: 'michael-rych', // Usually your GitHub org/user name.
     projectName: 'help.rych.io', // Usually your repo name.
 
+    plugins: [
+        [
+          '@docusaurus/plugin-ideal-image',
+          {
+            quality: 70,
+            max: 1030, // max resized image's size.
+            min: 640, // min resized image's size. if original is lower, use that size.
+            steps: 2, // the max number of images generated between min and max (inclusive)
+            disableInDev: true,
+          },
+        ],
+    ],
+
     i18n: {
         defaultLocale: 'en',
         locales: ['en', 'ms','zh-CN'],
